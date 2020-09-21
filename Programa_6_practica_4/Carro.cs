e0346d9d9a3f4fc83ffbe1fb85476d57e42b20cf
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:20:00 -0400

    Agregar archivos de proyecto.

---
+++ Programa_6_practica_4/Carro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_6_practica_4
+{
+   public  class Carro
+    {
+         private int cantidad_combustible;
+        
+
+        public void encender()
+        {
+
+            Console.WriteLine("Digite la cantidad de Combustible:");
+            cantidad_combustible = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i > cantidad_combustible; i++)
+            {
+                
+                                       }
+            if (cantidad_combustible >= 1)
+            {
+                Console.WriteLine("El auto esta encendido");
+
+                Console.WriteLine("El Carro pudo avanzar");
+
+            }
+            else if (cantidad_combustible < 1)
+            {
+                Console.WriteLine("Conbustible insuficiente" + cantidad_combustible);
+            }
+            Console.ReadKey();
+            return;
+           
+
+
+        }
+    }
+   
+}
---
+++ Programa_6_practica_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_6_practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro corre = new Carro();
+            corre.encender();
+        }
+    }
+}
